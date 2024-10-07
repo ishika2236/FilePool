55ee6cd450c5b37c642edce310ca31d4c5942ee7
Kartikey <bartwalkartikey1@gmail.com>
2024-10-07 12:41:51 +0530

    updated struct file

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -3,24 +3,29 @@ pragma solidity ^0.8.0;
 contract MyContract {
     
     struct File {
-        string ipfsHash; 
-        address uploader; 
+        string ipfsHash;  // HASH OF THE FILE IN IPFS
+        address uploader; // WHO UPLOADED THE FILE
+        uint256 fileSize; // FILE SIZE IN BYTES
+        uint256 timestamp; // TIMESTAMP OF UPLOAD
     }
 
     File[] public files;
 
-    event FileUploaded(string ipfsHash, address indexed uploader);
+    event FileUploaded(string ipfsHash, address indexed uploader, uint256 fileSize, uint256 timestamp);
 
-    function storeFile(string memory _ipfsHash) public {
-        files.push(File(_ipfsHash, msg.sender));
+    function storeFile(string memory _ipfsHash, uint256 _fileSize) public {
+        uint256 _timestamp = block.timestamp;
 
-        emit FileUploaded(_ipfsHash, msg.sender);
+        files.push(File(_ipfsHash, msg.sender, _fileSize, _timestamp));
+
+        emit FileUploaded(_ipfsHash, msg.sender, _fileSize, _timestamp);
     }
 
-    function getFile(uint256 _index) public view returns (string memory, address) {
+    function getFile(uint256 _index) public view returns (string memory, address, uint256, uint256) {
         require(_index < files.length, "File does not exist");
 
-        return (files[_index].ipfsHash, files[_index].uploader);
+        File memory file = files[_index];
+        return (file.ipfsHash, file.uploader, file.fileSize, file.timestamp);
     }
 
     function getFileCount() public view returns (uint256) {
