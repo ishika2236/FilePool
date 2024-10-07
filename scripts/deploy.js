bb1321dd879a8b2539cc523801383d4b8958c51f
Kartikey <bartwalkartikey1@gmail.com>
2024-10-07 12:16:53 +0530

    deploy js

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+const hre = require("hardhat");
+
+async function main() {
+
+    const [deployer] = await hre.ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const MyContract = await hre.ethers.getContractFactory("MyContract");
+
+    const myContract = await MyContract.deploy();
+
+    await myContract.deployed();
+
+    console.log("MyContract deployed to:", myContract.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
